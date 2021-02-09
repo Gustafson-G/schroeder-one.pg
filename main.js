1e1791c5562faebdc54f6ad7bdc38320cf68243e
Gustafson-G <76894514+Gustafson-G@users.noreply.github.com>
2021-02-08 21:03:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -88,3 +88,30 @@ sr.reveal('.work_img',{delay: 400});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact_input',{interval: 200}); 
+
+/* ARTWORK DIVIDE */
+let sortBtn = document.querySelector('.filter-menu').children;
+let sortItem = document.querySelector('.filter-item').children;
+
+for(let i = 0; i < sortBtn.length; i++){
+    sortBtn[i].addEventListener('click', function(){
+        for(let j = 0; j< sortBtn.length; j++){
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
+        
+
+        let targetData = this.getAttribute('data-target');
+
+        for(let k = 0; k < sortItem.length; k++){
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+
+            if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
